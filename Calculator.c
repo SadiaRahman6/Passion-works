da46ee080d77e17685ac4d6676d5cce6e1bb8285
Devhunt44k <140506771+Devhunt44k@users.noreply.github.com>
2023-08-01 09:10:27 +0600

    Add files via upload

---
+++ Calculator.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+// ANSI color escape codes
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
+int main() {
+    char operator;
+    double num1, num2, result;
+
+    printf("Enter the first number: ");
+    scanf("%lf", &num1);
+
+    printf("Enter the operator (+, -, *, /): ");
+    scanf(" %c", &operator);
+
+    printf("Enter the second number: ");
+    scanf("%lf", &num2);
+
+    switch (operator) {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            if (num2 != 0) {
+                result = num1 / num2;
+            } else {
+                printf("Error: Division by zero is not allowed.\n");
+                return 1;
+            }
+            break;
+        default:
+            printf("Error: Invalid operator.\n");
+            return 1;
+    }
+
+    printf("Result: ");
+    if (result >= 0) {
+        printf(ANSI_COLOR_GREEN);
+    } else {
+        printf(ANSI_COLOR_RED);
+    }
+    printf("%.2lf", result);
+    printf(ANSI_COLOR_RESET "\n");
+
+    return 0;
+}
